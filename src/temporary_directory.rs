78041f2686641f16deb3e6afacb91c9900fe9110
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-03-09 13:04:08 -0500

    Return a path that begins with system temporary directory

--- src/temporary_directory.rs
+++ src/temporary_directory.rs
@@ -1,6 +1,7 @@
+use std::env;
 use std::path::{Path, PathBuf};
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct TemporaryDirectory {
     path: PathBuf,
 }
@@ -17,9 +18,23 @@ impl TemporaryDirectory {
     }
 }
 
+impl Default for TemporaryDirectory {
+    fn default() -> Self {
+        let mut path = env::temp_dir();
+        path.push("tHiS dIr3cT0rY 1S uN1Ik3Ly T0 3x15t");
+        Self { path }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn it_should_return_a_path_that_begins_with_the_system_temporary_directory() {
+        let temp_dir = TemporaryDirectory::new();
+        assert!(temp_dir.get_path().starts_with(env::temp_dir()));
+    }
 
     #[test]
     fn it_should_return_a_path_that_does_not_exist_after_it_goes_out_of_scope() {
