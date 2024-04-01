3a1ddf7a353579498dbafb01771a1ee70d128cce
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-04-01 07:44:49 -0400

    Cache resolved sysfs paths

--- src/linux/sysfs.rs
+++ src/linux/sysfs.rs
@@ -2,48 +2,81 @@
 
 #[cfg(test)]
 use std::cell::OnceCell;
+use std::cell::{Ref, RefCell};
+use std::collections::HashMap;
 use std::fs;
 use std::io::Result;
 use std::path::{Path, PathBuf};
 
 /// Interface for reading and writing to kernel attributes using paths that are
 /// relative to the sysfs root directory.
-pub struct Sysfs;
+pub struct Sysfs {
+    path_cache: RefCell<HashMap<PathBuf, PathBuf>>,
+}
 
 impl Sysfs {
     /// Creates a new `Sysfs` interface.
     pub fn new() -> Self {
-        Self
+        Self {
+            path_cache: RefCell::new(HashMap::new()),
+        }
     }
 
     /// Read from a kernel attribute.
     pub fn read(&self, path: impl AsRef<Path>) -> Result<Vec<u8>> {
-        let path = self.get_sysfs_path(path);
-        fs::read(path)
+        let path_ref = self.resolve_path(path);
+        fs::read(path_ref.as_path())
     }
 
     /// Read from a kernel attribute into a [`String`].
     pub fn read_to_string(&self, path: impl AsRef<Path>) -> Result<String> {
-        let path = self.get_sysfs_path(path);
-        fs::read_to_string(path)
+        let path_ref = self.resolve_path(path);
+        fs::read_to_string(path_ref.as_path())
     }
 
     /// Write to a kernel attribute.
     pub fn write(&self, path: impl AsRef<Path>, contents: impl AsRef<[u8]>) -> Result<()> {
-        let path = self.get_sysfs_path(path);
-        fs::write(path, contents)
+        let path_ref = self.resolve_path(path);
+        fs::write(path_ref.as_path(), contents)
     }
 
-    fn get_sysfs_path(&self, attribute_path: impl AsRef<Path>) -> PathBuf {
+    fn cache_path(&self, attribute_path: PathBuf, path: PathBuf) {
+        self.path_cache.borrow_mut().insert(attribute_path, path);
+    }
+
+    fn get_cached_path(&self, path: &Path) -> Ref<'_, PathBuf> {
+        Ref::map(self.path_cache.borrow(), |cache| {
+            cache.get(path).expect("path should be cached")
+        })
+    }
+
+    fn has_cached_path(&self, path: &Path) -> bool {
+        self.path_cache.borrow().contains_key(path)
+    }
+
+    fn resolve_attribute_path(attribute_path: &Path) -> PathBuf {
         #[cfg(not(test))]
-        Path::new(Self::SYSFS_ROOT_DIR).join(path);
+        let path = Path::new(Self::SYSFS_ROOT_DIR).join(attribute_path);
 
         #[cfg(test)]
-        Self::SYSFS_ROOT_DIR.with(|cell| {
+        let path = Self::SYSFS_ROOT_DIR.with(|cell| {
             cell.get()
                 .expect("`SYSFS_ROOT_DIR` should be set by test")
                 .join(attribute_path)
-        })
+        });
+
+        path
+    }
+
+    fn resolve_path(&self, attribute_path: impl AsRef<Path>) -> Ref<'_, PathBuf> {
+        let attribute_path = attribute_path.as_ref();
+
+        if !self.has_cached_path(attribute_path) {
+            let path = Self::resolve_attribute_path(attribute_path);
+            self.cache_path(attribute_path.into(), path);
+        }
+
+        self.get_cached_path(attribute_path)
     }
 
     #[cfg(not(test))]
