31d642e2d1d00d4d817ef2713088829f4db4873c
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-04-01 08:24:34 -0400

    Allow injection of sysfs root dir

--- src/linux/sysfs.rs
+++ src/linux/sysfs.rs
@@ -1,7 +1,5 @@
 //! Interfaces for interacting with the Linux kernel sysfs.
 
-#[cfg(test)]
-use std::cell::OnceCell;
 use std::cell::{Ref, RefCell};
 use std::collections::HashMap;
 use std::fs;
@@ -10,15 +8,23 @@ use std::path::{Path, PathBuf};
 
 /// Interface for reading and writing to kernel attributes using paths that are
 /// relative to the sysfs root directory.
-pub struct Sysfs {
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Sysfs<'a> {
     path_cache: RefCell<HashMap<PathBuf, PathBuf>>,
+    root_dir: &'a Path,
 }
 
-impl Sysfs {
+impl<'a> Sysfs<'a> {
     /// Creates a new `Sysfs` interface.
     pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Creates a new `Sysfs` interface with a non-standard root directory.
+    pub fn with_root_dir(root_dir: &'a Path) -> Self {
         Self {
-            path_cache: RefCell::new(HashMap::new()),
+            root_dir,
+            ..Default::default()
         }
     }
 
@@ -54,37 +60,24 @@ impl Sysfs {
         self.path_cache.borrow().contains_key(path)
     }
 
-    fn resolve_attribute_path(attribute_path: &Path) -> PathBuf {
-        #[cfg(not(test))]
-        let path = Path::new(Self::SYSFS_ROOT_DIR).join(attribute_path);
-
-        #[cfg(test)]
-        let path = Self::SYSFS_ROOT_DIR.with(|cell| {
-            cell.get()
-                .expect("`SYSFS_ROOT_DIR` should be set by test")
-                .join(attribute_path)
-        });
-
-        path
-    }
-
     fn resolve_path(&self, attribute_path: impl AsRef<Path>) -> Ref<'_, PathBuf> {
         let attribute_path = attribute_path.as_ref();
 
         if !self.has_cached_path(attribute_path) {
-            let path = Self::resolve_attribute_path(attribute_path);
+            let path = self.root_dir.join(attribute_path);
             self.cache_path(attribute_path.into(), path);
         }
 
         self.get_cached_path(attribute_path)
     }
+}
 
-    #[cfg(not(test))]
-    const SYSFS_ROOT_DIR: &str = "/sys";
-
-    #[cfg(test)]
-    thread_local! {
-        static SYSFS_ROOT_DIR: OnceCell<PathBuf> = const { OnceCell::new() };
+impl<'a> Default for Sysfs<'a> {
+    fn default() -> Self {
+        Self {
+            path_cache: RefCell::new(HashMap::new()),
+            root_dir: Path::new("/sys"),
+        }
     }
 }
 
@@ -94,9 +87,14 @@ mod tests {
     use crate::unix::temporary_directory::TemporaryDirectory;
 
     #[test]
+    fn it_should_create_a_default_sysfs_interface() {
+        assert_eq!(Sysfs::new(), Sysfs::default());
+    }
+
+    #[test]
     fn it_should_read_from_an_attribute() {
-        let _sysfs_dir = mock_sysfs_dir();
-        let sysfs = Sysfs::new();
+        let sysfs_dir = mock_sysfs_dir();
+        let sysfs = Sysfs::with_root_dir(sysfs_dir.path());
         assert!(sysfs
             .read("class/pwm/pwmchip0/npwm")
             .is_ok_and(|contents| contents == NPWM.as_bytes()));
@@ -104,15 +102,15 @@ mod tests {
 
     #[test]
     fn it_should_return_an_error_when_reading_from_a_device_that_does_not_exist() {
-        let _sysfs_dir = mock_sysfs_dir();
-        let sysfs = Sysfs::new();
+        let sysfs_dir = mock_sysfs_dir();
+        let sysfs = Sysfs::with_root_dir(sysfs_dir.path());
         assert!(sysfs.read("class/pwm/pwmchip1/npwm").is_err());
     }
 
     #[test]
     fn it_should_read_from_an_attribute_to_a_string() {
-        let _sysfs_dir = mock_sysfs_dir();
-        let sysfs = Sysfs::new();
+        let sysfs_dir = mock_sysfs_dir();
+        let sysfs = Sysfs::with_root_dir(sysfs_dir.path());
         assert!(sysfs
             .read_to_string("class/pwm/pwmchip0/npwm")
             .is_ok_and(|contents| contents == NPWM));
@@ -120,15 +118,15 @@ mod tests {
 
     #[test]
     fn it_should_return_an_error_when_reading_to_a_string_from_a_device_that_does_not_exist() {
-        let _sysfs_dir = mock_sysfs_dir();
-        let sysfs = Sysfs::new();
+        let sysfs_dir = mock_sysfs_dir();
+        let sysfs = Sysfs::with_root_dir(sysfs_dir.path());
         assert!(sysfs.read_to_string("class/pwm/pwmchip1/npwm").is_err());
     }
 
     #[test]
     fn it_should_write_to_an_attribute() {
         let sysfs_dir = mock_sysfs_dir();
-        let sysfs = Sysfs::new();
+        let sysfs = Sysfs::with_root_dir(sysfs_dir.path());
         let channel = "0";
         sysfs
             .write("class/pwm/pwmchip0/export", channel)
@@ -139,19 +137,13 @@ mod tests {
 
     #[test]
     fn it_should_return_an_error_when_writing_to_an_attribute_for_a_device_that_does_not_exist() {
-        let _sysfs_dir = mock_sysfs_dir();
-        let sysfs = Sysfs::new();
+        let sysfs_dir = mock_sysfs_dir();
+        let sysfs = Sysfs::with_root_dir(sysfs_dir.path());
         assert!(sysfs.write("class/pwm/pwmchip1/export", "0").is_err());
     }
 
     fn mock_sysfs_dir() -> TemporaryDirectory {
         let sysfs_dir = TemporaryDirectory::new().expect("should succeed");
-
-        Sysfs::SYSFS_ROOT_DIR.with(|cell| {
-            cell.set(sysfs_dir.path().into())
-                .expect("`SYSFS_ROOT_DIR` should not be set");
-        });
-
         let pwm_controller_path = sysfs_dir.path().join("class/pwm/pwmchip0");
         fs::create_dir_all(&pwm_controller_path).expect("parent directory should be writable");
         let export_path = pwm_controller_path.join("export");
